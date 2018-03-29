7f4656d5688a7c5073e18902dd9ab1273bb30bf0
Anders Selhammer <anders.selhammer@est.tech>
2018-03-29 10:04:16 +0200

    pm: Added pm.c including pm storage handling

--- pm.h
+++ pm.h
@@ -60,7 +60,7 @@ struct pm_head {
 
 /* E2E and P2P */
 struct pm_clock_stats {
-	TAILQ_ENTRY(clock_pm_stats) list;
+	TAILQ_ENTRY(pm_clock_stats) list;
 	struct pm_head      head;
 	UInteger8           measurementValid;
 	UInteger8           periodComplete;
@@ -72,28 +72,105 @@ struct pm_clock_stats {
 
 /* P2P only */
 struct pm_port_stats {
-	TAILQ_ENTRY(port_pm_stats) list;
+	TAILQ_ENTRY(pm_port_stats) list;
 	struct pm_head      head;
 	struct stats        *meanLinkDelay;
 };
 
 /* E2E and P2P */
 struct pm_port_counters {
-	TAILQ_ENTRY(port_pm_counters) list;
+	TAILQ_ENTRY(pm_port_counters) list;
 	struct pm_head      head;
 	UInteger32          counter[N_MSG_COUNTERS];
 };
 
 struct pm_clock_recordlist {
-	TAILQ_HEAD(clock_pm_15_stats_head, clock_pm_stats) record15_stats;
-	TAILQ_HEAD(clock_pm_24_stats_head, clock_pm_stats) record24_stats;
+	TAILQ_HEAD(pm_clock_15_stats_head, pm_clock_stats) record15_stats;
+	TAILQ_HEAD(pm_clock_24_stats_head, pm_clock_stats) record24_stats;
 };
 
 struct pm_port_recordlist {
-	TAILQ_HEAD(port_pm_15_stats_head, port_pm_stats) record15_stats;
-	TAILQ_HEAD(port_pm_24_stats_head, port_pm_stats) record24_stats;
-	TAILQ_HEAD(port_pm_15_counters_head, port_pm_counters) record15_cnt;
-	TAILQ_HEAD(port_pm_24_counters_head, port_pm_counters) record24_cnt;
+	TAILQ_HEAD(pm_port_15_stats_head, pm_port_stats) record15_stats;
+	TAILQ_HEAD(pm_port_24_stats_head, pm_port_stats) record24_stats;
+	TAILQ_HEAD(pm_port_15_counters_head, pm_port_counters) record15_cnt;
+	TAILQ_HEAD(pm_port_24_counters_head, pm_port_counters) record24_cnt;
 };
+
+/**
+ * Creates stats instances for clock statistics.
+ * @param cr Handle to current record.
+ * @return   Zero on success, non-zero if the message is invalid.
+ */
+int pm_create_clock_stats(struct pm_clock_stats *cr);
+
+/**
+ * Creates stats instances for port statistics.
+ * @param cr Handle to current record.
+ * @return   Zero on success, non-zero if the message is invalid.
+ */
+int pm_create_port_stats(struct pm_port_stats *cr);
+
+/**
+ * Reset the stats instances for clock statistics.
+ * @param cr Handle to current record.
+ */
+void pm_reset_clock_stats(struct pm_clock_stats *cr);
+
+/**
+ * Reset the stats instances for port statistics.
+ * @param cr Handle to current record.
+ */
+void pm_reset_port_stats(struct pm_port_stats *cr);
+
+/**
+ * Destroys stats instances for clock statistics.
+ * @param cr Handle to current record.
+ */
+void pm_destroy_clock_stats(struct pm_clock_stats *cr);
+
+/**
+ * Destroys stats instances for port statistics.
+ * @param cr Handle to current record.
+ */
+void pm_destroy_port_stats(struct pm_port_stats *cr);
+
+/**
+ * Clear the record list and frees all the memory.
+ * @param rl Handle to clock recordlist.
+ */
+void pm_free_clock_recordlist(struct pm_clock_recordlist *rl);
+
+/**
+ * Clear the record list and frees all the memory.
+ * @param rl Handle to port recordlist.
+ */
+void pm_free_port_recordlist(struct pm_port_recordlist *rl);
+
+/**
+ * Update clock stats 15 minutes and 24 hour recordlist.
+ * @param cr Handle to current record to store.
+ * @param rl Handle to recordlist.
+ * @return   Zero on success, non-zero if the message is invalid.
+ */
+int pm_update_clock_stats_recordlist(struct pm_clock_stats *cr,
+				     struct pm_clock_recordlist *rl);
+
+/**
+ * Update port stats 15 minutes and 24 hour recordlist.
+ * @param cr Handle to current record to store.
+ * @param rl Handle to recordlist.
+ * @return   Zero on success, non-zero if the message is invalid.
+ */
+int pm_update_port_stats_recordlist(struct pm_port_stats *cr,
+				    struct pm_port_recordlist *rl);
+
+/**
+ * Update port counters 15 minutes and 24 hour recordlist.
+ * @param cr Handle to current record to store.
+ * @param rl Handle to recordlist.
+ * @return   Zero on success, non-zero if the message is invalid.
+ */
+int pm_update_port_counters_recordlist(struct pm_port_counters *cr,
+				       struct pm_port_recordlist *rl);
 
 #endif
